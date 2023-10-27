ce49d15ab0d8290360cd551f0a88f309b1568842
Samuele Musiani <samuele.musiani.03@gmail.com>
2023-10-28 00:09:04 +0200

    New parameters in config

--- config/config.go
+++ config/config.go
@@ -6,35 +6,51 @@ import (
 	"github.com/ghodss/yaml"
 )
 
-type LdapSearch struct {
-	BaseDN                string `json:"baseDN"`
-	Filter                string `json:"filter"`
-	Username              string `json:"username"`
-	IdAttr                string `json:"idAttr"`
-	EmailAttr             string `json:"emailAttr"`
-	NameAttr              string `json:"nameAttr"`
-	PreferredUsernameAttr string `json:"preferredUsernameAttr"`
-}
-
 type LdapConfig struct {
-	Uri                string     `json:"uri"`
-	Port               int        `json:"port"`
-	BindDN             string     `json:"bindDN"`
-	BindPW             string     `json:"bindPW"`
-	StartTLS           bool       `json:"startTLS"`
-	InsecureNoSSL      bool       `json:"insecureNoSSL"`
-	InsecureSkipVerify bool       `json:"insecureSkipVerify"`
-	LdapSearch         LdapSearch `json:"userSearch"`
+	Uri                string `json:"uri"`
+	Port               int    `json:"port"`
+	BindDN             string `json:"bindDN"`
+	BindPW             string `json:"bindPW"`
+	StartTLS           bool   `json:"startTLS"`
+	InsecureNoSSL      bool   `json:"insecureNoSSL"`
+	InsecureSkipVerify bool   `json:"insecureSkipVerify"`
 }
 
 type WebConfig struct {
-	Http string `json:"http"`
+	Ip   string `json:"ip"`
 	Port string `json:"port"`
 }
 
+type Server struct {
+	Address            string `json:"address"`
+	Username           string `json:"username"`
+	Password           string `json:"password"`
+	StartTLS           bool   `json:"startTLS"`
+	InsecureSkipVerify bool   `json:"insecureSkipVerify"`
+}
+
+type Sender struct {
+	Address  string `json:"address"`
+	Template string `json:"template"`
+}
+
+type Email struct {
+	Server Server `json:"server"`
+	Sender Sender `json:"sender"`
+}
+
+type Modules struct {
+	Root    string   `json:"root"`
+	Enabled []string `json:"enabled"`
+}
+
 type Config struct {
-	WebConfig  WebConfig  `json:"web"`
-	LdapConfig LdapConfig `json:"ldap"`
+	WebConfig          WebConfig  `json:"web"`
+	LdapConfig         LdapConfig `json:"ldap"`
+	LoginScript        string     `json:"login"`
+	AllowPasswordReset bool       `json:"allow_password_reset"`
+	Email              Email      `json:"email"`
+	Modules            Modules    `json:"modules"`
 }
 
 func ParseConfig(configPath string) (Config, error) {
--- config/config_test.go
+++ config/config_test.go
@@ -9,7 +9,7 @@ import (
 func TestParseConfigWeb(t *testing.T) {
 	conf, err := ParseConfig("./config.yaml")
 	assert.NilError(t, err)
-	assert.Equal(t, conf.WebConfig.Http, "0.0.0.0")
+	assert.Equal(t, conf.WebConfig.Ip, "0.0.0.0")
 	assert.Equal(t, conf.WebConfig.Port, "8080")
 }
 
@@ -25,16 +25,38 @@ func TestParseConfigLdap(t *testing.T) {
 	assert.Equal(t, conf.LdapConfig.InsecureSkipVerify, false)
 }
 
-func TestParseConfigLdapSearch(t *testing.T) {
+func TestParseConfigLogin(t *testing.T) {
 	conf, err := ParseConfig("./config.yaml")
 	assert.NilError(t, err)
-	assert.Equal(t, conf.LdapConfig.LdapSearch.BaseDN,
-		"ou=users,dc=example,dc=com")
-	assert.Equal(t, conf.LdapConfig.LdapSearch.Filter,
-		"(objectClass=inetOrgPerson)")
-	assert.Equal(t, conf.LdapConfig.LdapSearch.Username, "cn")
-	assert.Equal(t, conf.LdapConfig.LdapSearch.IdAttr, "dn")
-	assert.Equal(t, conf.LdapConfig.LdapSearch.EmailAttr, "mail")
-	assert.Equal(t, conf.LdapConfig.LdapSearch.NameAttr, "givenName")
-	assert.Equal(t, conf.LdapConfig.LdapSearch.PreferredUsernameAttr, "cn")
+	assert.Equal(t, conf.LoginScript, "path/to/check/script")
+}
+
+func TestParseConfigAllowPasswordReset(t *testing.T) {
+	conf, err := ParseConfig("./config.yaml")
+	assert.NilError(t, err)
+	assert.Equal(t, conf.AllowPasswordReset, true)
+}
+
+func TestParseConfigEmailServer(t *testing.T) {
+	conf, err := ParseConfig("./config.yaml")
+	assert.NilError(t, err)
+	assert.Equal(t, conf.Email.Server.Address, "host:port")
+	assert.Equal(t, conf.Email.Server.Username, "user")
+	assert.Equal(t, conf.Email.Server.Password, "password")
+	assert.Equal(t, conf.Email.Server.StartTLS, true)
+	assert.Equal(t, conf.Email.Server.InsecureSkipVerify, false)
+}
+
+func TestParseConfigEmailSender(t *testing.T) {
+	conf, err := ParseConfig("./config.yaml")
+	assert.NilError(t, err)
+	assert.Equal(t, conf.Email.Sender.Address, "root@teapot.ovh")
+	assert.Equal(t, conf.Email.Sender.Template, "path/to/template")
+}
+
+func TestParseConfigModules(t *testing.T) {
+	conf, err := ParseConfig("./config.yaml")
+	assert.NilError(t, err)
+	assert.Equal(t, conf.Modules.Root, "path/to/modules")
+	assert.DeepEqual(t, conf.Modules.Enabled, []string{"password", "email"})
 }
