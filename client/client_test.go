d7d65dcd3e4f197cb2258acca1edec7dc092e941
Samuele Musiani <samuele.musiani.03@gmail.com>
2023-10-29 13:13:37 +0100

    Tests for client

--- client/client_test.go
+++ client/client_test.go
@@ -7,9 +7,32 @@ import (
 	"gotest.tools/v3/assert"
 )
 
-func TestParseConfigWeb(t *testing.T) {
+func TestStartLdaps(t *testing.T) {
 	err := config.ParseConfig("../config/config.yaml")
 	assert.NilError(t, err)
+	conf := config.Get()
+	conf.LdapConfig.Address = "ldaps://ldap.google.com:636"
+	_, err = Start()
+	assert.NilError(t, err)
+}
+
+func TestStartLdapNoStartTLS(t *testing.T) {
+	err := config.ParseConfig("../config/config.yaml")
+	assert.NilError(t, err)
+	conf := config.Get()
+	conf.LdapConfig.Address = "ldap://ldap.google.com:389"
+	conf.LdapConfig.StartTLS = false
+	_, err = Start()
+	assert.NilError(t, err)
+}
+
+func TestStartLdapStartTLS(t *testing.T) {
+	err := config.ParseConfig("../config/config.yaml")
+	assert.NilError(t, err)
+	conf := config.Get()
+	conf.LdapConfig.Address = "ldap://ldap.google.com:389"
+	conf.LdapConfig.StartTLS = true
+	conf.LdapConfig.InsecureSkipVerify = true
 	_, err = Start()
 	assert.NilError(t, err)
 }
